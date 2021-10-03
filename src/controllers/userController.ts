b5806ddfe7fc55490e6d0da6250c455162fb8e64
Lukyanuk Vladislav <7ggamerussian@gmail.com>
2021-10-03 18:49:37 +0600

    make a user rotation

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,19 @@
-import {Camera, Euler} from "three";
+import {Camera, Euler, Quaternion} from "three";
 
 export class UserController {
     private readonly _camera: Camera;
     private readonly _domNode: Element;
     private readonly _euler: Euler;
-    private readonly _minPolarAngle = 0;
-    private readonly _maxPolarAngle = Math.PI;
+    private _isLocked: boolean;
+    private readonly _minPolarAngle = - Math.PI / 4;
+    private readonly _maxPolarAngle = Math.PI / 4;
 
     constructor(camera: Camera, domNode: Element) {
         this._camera = camera;
         this._domNode = domNode;
         this._euler = (new Euler()).reorder('XYZ');
-        debugger
+        this._isLocked = false;
 
-        this._lockPointer();
         this._connect();
     }
 
@@ -27,19 +27,31 @@ export class UserController {
     }
 
     private _lockPointer() {
+        if(this._isLocked) {
+            return;
+        }
+
         this._domNode.requestPointerLock();
+        this._isLocked = true;
     }
 
     private _unlockPointer() {
+        if(!this._isLocked) {
+            return;
+        }
+
         this._domNode.ownerDocument.exitPointerLock();
+        this._isLocked = true;
     }
 
     private _connect() {
-        this._domNode.addEventListener('mousemove', this._handleMouseMove);
+        this._domNode.addEventListener('mousemove', this._handleMouseMove.bind(this));
+        this._domNode.addEventListener('click', this._lockPointer.bind(this));
     }
 
     private _disconnect() {
-        this._domNode.removeEventListener('mousemove', this._handleMouseMove);
+        this._domNode.removeEventListener('mousemove', this._handleMouseMove.bind(this));
+        this._domNode.removeEventListener('click', this._lockPointer.bind(this));
     }
 
     private _handleMouseMove(event : MouseEvent) {
@@ -47,12 +59,14 @@ export class UserController {
         const movementY = event.movementY || 0;
 
         debugger
-        this._euler.setFromQuaternion(this._camera.quaternion);
-        this._euler.y -= movementX * 0.002;
-        this._euler.x -= movementY * 0.002;
+        let quaternion = new Quaternion().copy(this._camera.quaternion);
 
-        this._euler.x = Math.max( this._maxPolarAngle, Math.min( this._minPolarAngle, this._euler.x ) );
+        const turn = new Quaternion(movementY * 0.002, movementX * 0.002, 0);
+        quaternion.multiply(turn);
+        quaternion.normalize();
 
-        this._camera.quaternion.setFromEuler( this._euler );
+        quaternion.x = Math.min(this._maxPolarAngle, Math.max(this._minPolarAngle, quaternion.x));
+
+        this._camera.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import {BoxGeometry, Euler, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer} from 'three';
+import {
+    BoxGeometry,
+    Color,
+    Euler, Float32BufferAttribute, Fog, HemisphereLight,
+    Mesh, MeshBasicMaterial,
+    MeshNormalMaterial,
+    PerspectiveCamera, PlaneGeometry, Raycaster,
+    Scene,
+    Vector3,
+    WebGLRenderer
+} from 'three';
 import {UserController} from "./controllers/userController";
 
 let camera: PerspectiveCamera;
@@ -26,14 +36,65 @@ function animation( time ) {
 }
 
 (function () {
-    camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-    camera.position.z = 1;
+    camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.y = 10;
+
+    const raycaster = new Raycaster( new Vector3(), new Vector3( 0, - 1, 0 ), 0, 10 );
+
+    // floor
+
+    let floorGeometry = new PlaneGeometry( 2000, 2000, 100, 100 );
+    floorGeometry.rotateX( - Math.PI / 2 );
+
+    // vertex displacement
+
+    let position = floorGeometry.attributes.position;
+
+    for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+        const vertex = new Vector3().fromBufferAttribute( position, i );
+
+        vertex.x += Math.random() * 20 - 10;
+        vertex.y += Math.random() * 2;
+        vertex.z += Math.random() * 20 - 10;
+
+        position.setXYZ( i, vertex.x, vertex.y, vertex.z );
+
+    }
+
+    // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+    position = floorGeometry.attributes.position;
+    const colorsFloor = [];
+
+    for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+        const color = new Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+        colorsFloor.push( color.r, color.g, color.b );
+
+    }
+
+    floorGeometry.setAttribute( 'color', new Float32BufferAttribute( colorsFloor, 3 ) );
+
+    const floorMaterial = new MeshBasicMaterial( { vertexColors: true } );
+
+    const floor = new Mesh( floorGeometry, floorMaterial );
+
+
+    const light = new HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+    light.position.set( 0.5, 1, 0.75 );
 
     scene = new Scene();
 
+    scene.add( floor );
+    scene.add( light );
+
+
     const controller = new UserController(camera, document.body);
 
-    scene.add(controller.detObject())
+    scene.add(controller.detObject());
+    scene.background = new Color( 0xffffff );
+    scene.fog = new Fog( 0xffffff, 0, 750 );
 
     const geometry = new BoxGeometry( 0.5, 0.2, 0.2 );
     const material = new MeshNormalMaterial();
